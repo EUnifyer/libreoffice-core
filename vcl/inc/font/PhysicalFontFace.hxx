3efc8b5ca75fa4b69da102892c93942aa68bc083
Khaled Hosny <khaled@aliftype.com>
2026-04-03 23:56:38 +0200

    tdf#152206: Add FontVariationAxis and GetVariationAxes API
    Add API to get font variation axes supported by a font. We will use it
    later for setting up font variations UI sliders.
    
    Change-Id: I7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c

--- vcl/inc/font/PhysicalFontFace.hxx
+++ vcl/inc/font/PhysicalFontFace.hxx
@@ -193,6 +193,8 @@ public:
     virtual const std::vector<vcl::font::Variation>&
     GetVariations(const LogicalFontInstance&) const;
 
+    bool GetVariationAxes(std::vector<vcl::font::VariationAxis>& rAxes) const;
+
 protected:
     mutable hb_face_t* mpHbFace;
     mutable hb_font_t* mpHbUnscaledFont;
@@ -200,6 +202,7 @@ protected:
     mutable std::optional<vcl::FontCapabilities> mxFontCapabilities;
     mutable std::optional<std::vector<ColorPalette>> mxColorPalettes;
     mutable std::optional<std::vector<vcl::font::Variation>> mxVariations;
+    mutable std::optional<std::vector<vcl::font::VariationAxis>> mxVariationAxes;
 
     explicit PhysicalFontFace(const FontAttributes&);
 
